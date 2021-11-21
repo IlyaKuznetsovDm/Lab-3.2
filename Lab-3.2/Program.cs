18dea0899fd8a2391a67e07642d8c3c2c63df5bc
Dmitr <Dmitr@DESKTOP-H2P4N61>
2021-11-21 18:33:17 +0200

    Добавьте файлы проекта.

---
+++ Lab-3.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //3.2 - базовый уровень - 8 вариант 
+            try
+            {             
+                int i = 2;
+                Console.WriteLine("Введите число:");
+                double n = double.Parse(Console.ReadLine());
+                Console.Write(" {0} = ", n);
+                while (n > 1) 
+                {
+                    while (n % i == 0)
+                    {
+                        Console.Write("{0} * ", i);
+                        n /= i;
+                    }
+                    if (i == 2) i++;
+                    else i += 2;
+
+                }
+               
+
+            }
+            catch (Exception)
+            { 
+            
+                Console.WriteLine("Введите корректно переменную");
+            }
+           
+               
+            
+        }
+    }
+}
